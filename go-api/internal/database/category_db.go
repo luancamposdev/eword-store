85628e1739ac1deb903cc19127c8ff7a0b158ded
Luan Campos <luancamposproducoes@gmail.com>
2024-02-09 13:05:35 -0300

    chore: create list categories from db

---
+++ go-api/internal/database/category_db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/luancamposdev/eword-store/go-api/internal/entity"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{
+		db: db,
+	}
+}
+
+func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := cd.db.Query("SELECR id, name FROM categories")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categories []*entity.Category
+	for rows.Next() {
+		var category entity.Category
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, &category)
+	}
+
+	return categories, nil
+}
