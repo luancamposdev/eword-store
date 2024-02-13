4aa1e7796a84759c01c9ed3ef16454508d7a6ecf
Luan Campos <luancamposproducoes@gmail.com>
2024-02-13 02:19:29 -0300

    chore: create application main

---
+++ go-api/cmd/catalog/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/luancamposdev/eword-store/go-api/internal/database"
+	"github.com/luancamposdev/eword-store/go-api/internal/service"
+	"github.com/luancamposdev/eword-store/go-api/internal/webserver"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:unix@tcp(localhost:3306)/eword")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHandler := webserver.NewWebProducthandler(productService)
+
+	c := chi.NewRouter()
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+
+	c.Get("/category/{id}", webCategoryHandler.GetCategory)
+	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product/{id}", webProductHandler.GetProduct)
+	c.Get("/product", webProductHandler.GetProducts)
+	c.Get("/product/category/{categoryID}", webProductHandler.GetProductByCategoryID)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("🔥Server is running on port 3331")
+	http.ListenAndServe(":3331", c)
+}
