cd1c284ff8abbeaaf4886a4ccb3743dcc3cc6d3b
Luan Campos <luancamposproducoes@gmail.com>
2024-02-21 23:43:50 -0300

    chore: update

--- nest-api/src/fixture.ts
+++ nest-api/src/fixture.ts
@@ -13,39 +13,39 @@ async function bootstrap() {
   const productRepo = dataSource.getRepository('Product');
   await productRepo.insert([
     {
-      id: '8c2d6919-b9ef-41da-8459-a9cae53e762c',
+      id: '8c2d6912-b9ef-41da-8459-a9cae53e762c',
       name: 'Product 1',
       description: 'Product 1 description',
-      image_url: 'http://via.placeholder.com/158',
       price: 135,
+      image_url: 'http://via.placeholder.com/158',
     },
     {
-      id: 'fa34b4b9-08ab-46a4-8c73-36f7e7388fae',
+      id: 'fa34g4b9-08ab-46a4-8c73-36f7e7388fae',
       name: 'Product 2',
       description: 'Product 2 description',
-      image_url: 'http://via.placeholder.com/150',
       price: 250,
+      image_url: 'http://via.placeholder.com/150',
     },
     {
-      id: '1149d5be-a050-4dbf-b2ab-68cc59481da9',
+      id: '4549d5be-a050-4dbf-b2ab-68cc59481da9',
       name: 'Product 3',
       description: 'Product 3 description',
-      image_url: 'http://via.placeholder.com/150',
       price: 250,
+      image_url: 'http://via.placeholder.com/150',
     },
     {
-      id: '2eb629bb-df16-4319-bfdd-bda5e75c85d1',
+      id: '2tb629bb-df16-4319-bfdd-bda5e75c85d1',
       name: 'Product 4',
       description: 'Product 4 description',
-      image_url: 'http://via.placeholder.com/150',
       price: 250,
+      image_url: 'http://via.placeholder.com/150',
     },
     {
-      id: '21e61afc-2584-4a1f-9fcc-cfeb8bfdd1fb',
+      id: '21l61afc-2584-4a1f-9fcc-cfeb8bfdd1fb',
       name: 'Product 5',
       description: 'Product 5 description',
-      image_url: 'http://via.placeholder.com/150',
       price: 250,
+      image_url: 'http://via.placeholder.com/150',
     },
   ]);
 
