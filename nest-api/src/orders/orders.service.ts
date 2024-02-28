89ff40145508ec1f1d7abd7eeafff79ced0061ca
Luan Campos <luancamposproducoes@gmail.com>
2024-02-28 12:35:23 -0300

    chore: create order

--- nest-api/src/orders/orders.service.ts
+++ nest-api/src/orders/orders.service.ts
@@ -4,14 +4,16 @@ import { Product } from 'src/products/entities/product.entity';
 import { In, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Order } from './entities/order.entity';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order) private orderRepo: Repository<Order>,
     @InjectRepository(Product) private productRepo: Repository<Product>,
+    private amqpConnection: AmqpConnection,
   ) {}
-  async create(createOrderDto: CreateOrderDto) {
+  async create(createOrderDto: CreateOrderDto & { client_id: string }) {
     const productIds = createOrderDto.items.map((item) => item.product_id);
     const uniqueProductIds = [...new Set(productIds)];
     const products = await this.productRepo.findBy({
@@ -25,7 +27,7 @@ export class OrdersService {
     }
 
     const order = Order.create({
-      client_id: '123138484dwdqdwef_rgregre',
+      client_id: createOrderDto.client_id,
       items: createOrderDto.items.map((item) => {
         const product = products.find(
           (product) => product.id === item.product_id,
@@ -38,14 +40,25 @@ export class OrdersService {
       }),
     });
     await this.orderRepo.save(order);
+    await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+      order_id: order.id,
+      card_hash: createOrderDto.card_hash,
+      total: order.total,
+    });
     return order;
   }
 
-  findAll() {
-    return this.orderRepo.find();
+  findAll(client_id: string) {
+    return this.orderRepo.find({
+      where: { client_id },
+      order: { created_at: 'DESC' },
+    });
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} order`;
+  findOne(id: string, client_id: string) {
+    return this.orderRepo.findOneByOrFail({
+      id,
+      client_id,
+    });
   }
 }
